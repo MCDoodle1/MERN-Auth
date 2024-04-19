ccfffb82eb8061fab631c829889636e998c38c79
MCDoodle1 <mclarijs@me.com>
2024-04-19 16:27:11 +0200

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const cors = require("cors");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
@@ -15,13 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://mern-auth-04na.onrender.com"],
-  })
-);
-
-/* // Serve frontend
+// Serve frontend
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) => {
@@ -31,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     res.send("Please set to production");
   });
-} */
+}
 
 app.use(errorHandler);
 
