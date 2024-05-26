b7ec2d667bc7f40e50f1f7c850f27b5a2cac1509
MCDoodle1 <mclarijs@me.com>
2024-05-26 21:20:15 +0200

    Prepare for deployment to render

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
@@ -16,14 +18,13 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  app.use(express.static(path.join(__dirname, "../", "frontend", "build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
 } else {
-  app.get("/", (req, res) => {
-    res.send("Please set to production");
-  });
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
 
 app.use(errorHandler);
